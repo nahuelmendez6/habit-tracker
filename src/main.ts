6a5bec8617aa8955609bddc27d24a1b8af779443
nahuelmendez6 <mendezmaturano6@gmail.com>
2026-04-08 16:00:00 +0200

    core: ValidationPipe configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import { ValidationPipe } from '@nestjs/common';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Habilita validación global
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,  //Elimina propiedades no definidas en el DTO
+      forbidNonWhitelisted: true, // Lanza error si llegan propiedades extra
+      transform: true, // convierte automaticamente los tipos 
+    })
+  )
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
