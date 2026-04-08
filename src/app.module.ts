2b69c046ee7b282c1cedafead50963f957539d01
nahuelmendez6 <mendezmaturano6@gmail.com>
2026-04-08 15:51:20 +0200

    core: database configuration. - TypeOrm config for sqlite3

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
+
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: join(__dirname, '..', 'data', 'database.sqlite'),
+      entities: [__dirname + '/**/*.entity{.ts,.js'],
+      synchronize: true,
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
