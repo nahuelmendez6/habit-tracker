59adfb5bbc652e17d5521d03897ca79136de7bf5
nahuelmendez6 <mendezmaturano6@gmail.com>
2026-04-13 17:36:08 +0200

    feat: implement jwt.strategy

---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,24 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy, ExtractJwt } from 'passport-jwt';
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(config: ConfigService) {
+    
+    const secret = config.getOrThrow<string>('JWT_SECRET');
+    
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: secret,
+    });
+  }
+
+  validate(payload: any) {
+    return {
+      userId: payload.sub,
+      email: payload.email,
+    };
+  }
+}
